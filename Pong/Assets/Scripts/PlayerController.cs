70d883f11ba4f834cf462fd40faf6d0056c73cfe
Heitor Hellou <heitor.hellou@outlook.com>
2020-07-27 14:47:29 -0300

    Play area and Player

---
+++ Pong/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+  public int speed;
+
+  private Rigidbody rb;
+
+  private void Start()
+  {
+    rb = GetComponent<Rigidbody>();
+  }
+
+  private void FixedUpdate()
+  {
+    float move = Input.GetAxis("Vertical");
+
+    Vector3 movement = new Vector3(0.0f, 0.0f, move);
+
+    rb.velocity = movement * speed;
+  }
+}
