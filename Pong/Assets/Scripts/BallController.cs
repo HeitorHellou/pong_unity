f5293aa530e97b420a9b05c75c5d16f3e342f485
Heitor Hellou <heitor.hellou@outlook.com>
2020-07-29 17:42:13 -0300

    Ball

---
+++ Pong/Assets/Scripts/BallController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+  public float speed;
+
+  Rigidbody rb;
+
+  private void Start()
+  {
+    rb = GetComponent<Rigidbody>(); // Linking the rigidbody
+    LaunchBall(); // Launching the ball
+    Debug.Log(rb.velocity.magnitude);
+  }
+
+  // Adjusting the ball speed and direction after colliding with an object 
+  private void OnCollisionEnter(Collision collision)
+  {
+    // Since the wall are above or below the ball make the calcs with the z axis
+    if (collision.gameObject.tag == "Wall")
+    {
+      if (transform.position.z > collision.transform.position.z)
+      {
+        rb.AddForce(transform.forward.normalized * speed, ForceMode.Impulse);
+      }
+      if (transform.position.z < collision.transform.position.z)
+      {
+        rb.AddForce(transform.forward.normalized * -speed, ForceMode.Impulse);
+      }
+    }
+
+    // As the player will be on it's side (right, or left) make the calcs with the x axis
+    if (collision.gameObject.tag == "Player")
+    {
+      if (transform.position.x > collision.transform.position.x)
+      {
+        rb.AddForce(transform.right.normalized * speed, ForceMode.Impulse);
+      }
+      if (transform.position.x < collision.transform.position.x)
+      {
+        rb.AddForce(transform.right.normalized * -speed, ForceMode.Impulse);
+      }
+    }
+    Debug.Log(rb.velocity.magnitude);
+  }
+
+  public void LaunchBall()
+  {
+    rb.velocity = new Vector3(-1.0f, 0.0f, Random.Range(-3.0f, 3.0f)) * speed;
+  }
+}
--- Pong/Assets/Scripts/PlayerController.cs
+++ Pong/Assets/Scripts/PlayerController.cs
@@ -4,29 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-  public float speed, zMin, zMax;
+  public float speed;
 
-  private Rigidbody rb;
-
-  private void Start()
-  {
-    rb = GetComponent<Rigidbody>();
-  }
+  float zMin = -9, zMax = 9;
 
   private void FixedUpdate()
   {
     // Moving the player
     // Since the player is Kinematic a few steps are necessary to assure it will move
-    float moveZ = Input.GetAxis("Vertical") * speed; // Getting input from the player to move the paddle multiplied by it's speed
+    float moveZ = Input.GetAxis("Vertical"); // Getting input from the player to move the paddle 
 
     Vector3 pos = new Vector3(transform.position.x, transform.position.y, transform.position.z); // Getting the current paddle possition
     pos.z = moveZ; // Assining the movement on the z axis, since the camera is top - down
 
-    transform.Translate(0, 0, pos.z); // Moving the paddle
+    transform.Translate(0, 0, pos.z * speed * Time.fixedDeltaTime); // Moving the paddle
     CheckBoundaries(); // Checking if the player didn't reach the top nor the bottom of the screen
   }
 
-  // Checking the limits within the player can move
+  // Checking the limits in which the player can move
   public void CheckBoundaries()
   {
     if (transform.position.z < zMin)
