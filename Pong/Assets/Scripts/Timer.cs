0d2a8349d2d9d6a15a9d453500f00205304a6ac8
Othon <othonvvalentim@gmail.com>
2020-12-24 14:25:11 -0300

    Timer

---
+++ Pong/Assets/Scripts/Timer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+  public TextMeshProUGUI timer;
+  public float startTime;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    startTime = Time.time;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    float t = Time.time - startTime;
+    string seconds = (t % 60).ToString("f1");
+
+    timer.text = seconds;
+    }
+}
